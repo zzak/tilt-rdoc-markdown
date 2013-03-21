adffc500a482f167f93d91b30e41e06f0eae7c54
Zachary Scott <zachary@zacharyscott.net>
2013-03-21 00:10:23 -0400

    should probably commit lib

---
+++ lib/tilt/rdoc/markdown.rb
@@ -0,0 +1,25 @@
+require "rdoc"
+require "tilt"
+require "tilt/template"
+
+module Tilt
+  module RDoc
+    class Markdown < ::Tilt::Template
+      VERSION = "0.0.1"
+
+      def prepare # :nodoc:
+        @engine = ::RDoc::Markdown.new
+        @output = nil
+      end
+
+      def evaluate(scope, locals, &block) # :nodoc:
+        opts = ::RDoc::Options.new
+        opts.pipe = true # TODO: find a good way to expose this to tilt users
+        formatter = ::RDoc::Markup::ToHtml.new(opts, nil)
+        @output ||= @engine.parse(data).accept(formatter)
+      end
+    end
+  end
+
+  register RDoc::Markdown, 'markdown', 'mkd', 'md'
+end
